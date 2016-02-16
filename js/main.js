90b3ac64443ee1f510542656094e7d30426d7f32
Tomusdrw <tomusdrw@gmail.com>
2016-02-16 09:14:12 +0100

    Dodajemy 'Model' naszej aplikacji [Model]

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-// Na razie wypiszemy tylko info na konsolę
-console.log('Hello tasks app.');
+// Używamy trybu `strict`
+"use strict";
+
+var now = new Date().getTime();
+
+//14/ Przygotowujemy model zadań do wykonania
+var tasks = [
+  {
+    // zapisujemy nazwę zadania...
+    name: 'ModernJS training',
+    // oraz czas, w którym zostało rozpoczęte
+    started: now - 1000 * 60 * 10
+  },
+  //5/ a dla skończonych zadań trzymamy też timestamp ukończenia
+  {
+    name: 'Learning JavaScript',
+    started: now - 1000 * 3600 * 24,
+    finished: now - 1000 * 3600 * 6
+  }
+];
+
+// Kolejnym krokiem będzie wyświetlenie zadań.
+console.log('Tasks', tasks);
