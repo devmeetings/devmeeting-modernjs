a2ff1818e47be73657f6f876ba75e8be652f486f
Tomusdrw <tomusdrw@gmail.com>
2016-02-20 20:30:35 +0100

    Funkcja pomocnicza fetch [Fetch]

--- src/main.js
+++ src/main.js
@@ -5,23 +5,11 @@ import {TasksView} from './views/TasksView';
 
 const model = new TasksModel([]);
 
-//15/ Pobierzmy dane z serwera za pomocą XMLHttpRequest
-{
-  const req = new XMLHttpRequest();
-  req.open('GET', './data/tasks.json', true);
-  req.onreadystatechange = (evt) => {
-    if (req.readyState !== 4) {
-      return;
-    }
-
-    if (req.status !== 200) {
-      console.error('Error while fetching tasks.');
-      return;
-    }
-    model.tasks = JSON.parse(req.responseText);
-  };
-  req.send(null);
-}
+//4/ 5. Używamy naszej pomocniczej funkcji
+fetch('data/tasks.json').then(
+  (tasks) => model.tasks = tasks,
+  (err) => console.error(err)
+);
 
 const activeView = new TasksView(
   document.querySelector('#todos'),
@@ -39,3 +27,26 @@ setInterval(() => {
   view.render(now);
   activeView.render(now);
 }, 500);
+
+//17/ 1. Pobieramy dany `url` i zwracamy `Promise`
+function fetch (url) {
+  // 2. Tworzymy nowy Promise
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.onreadystatechange = (evt) => {
+      if (req.readyState !== 4) {
+        return;
+      }
+
+      if (req.status !== 200) {
+        // 3. Odrzucamy jeśli jest jakiś błąd...
+        return reject(req.status);
+      }
+      // 4. ...i rozwiązujemy, jeśli wszystko OK (pomijamy błędy z JSONa)
+      return resolve(JSON.parse(req.responseText));
+    };
+    req.send(null);
+  });
+}
+
--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -1,12 +1,10 @@
 "use strict";
 
-//9/ Model będzie teraz tylko eksportował klasę
 export class TasksModel {
   constructor(tasks) {
     this._tasks = tasks;
   }
 
-  //3/ Użyjemy settera, aby zapamiętać zadania
   set tasks(tasks) {
     this._tasks = tasks;
   }
