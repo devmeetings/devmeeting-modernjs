29a3feb58c3f7b8c5056abec0ac21aefd19e7fc9
Tomusdrw <tomusdrw@gmail.com>
2016-02-20 20:44:57 +0100

    Destructuring Assignment i Template Strings [Destructuring]

--- src/views/TasksView.js
+++ src/views/TasksView.js
@@ -1,37 +1,36 @@
 "use strict";
 
-import * as _ from 'lodash';
-
 export class TasksView {
   
   constructor($app, model, options) {
     this.$app = $app;
     this.model = model;
     this.options = options;
-    this.tpl = _.template(
-      '<div class="task"><span><%= name %></span> <span><%= time %></span></div>'
-    );
   }
 
   render(now) {
+    // `Destructuring assignment` - rozbijamy obiekt
+    const {onlyActive} = this.options;
+
     this.$app.innerHTML = this.model.tasks
-      //3/ Przefiltrujmy kolekcje w zależności od opcji
       .filter((task) => {
-        return this.options.onlyActive ? !task.finished : true;
+        return onlyActive ? !task.finished : true;
       })
-      //3/ Z lambdą kod staje się bardziej przejrzysty i nie ma problemu z kontekstem
       .map((task) => this._renderTask(now, task))
       .join('\n');
   }
 
   _renderTask(now, task) {
-    const time = (((task.finished || now) - task.started) / 1000 / 60).toFixed(1);
+    const time = (((task.finished || now) - task.started) / 1000 / 60);
     const name = task.name; 
 
-    return this.tpl({
-        name,
-        time: `${time} min`
-    });
+    //6/ Dzięki Template strings możemy nawet zrezygnować z naszego templatu.
+    return `
+      <div class="task">
+        <span>${name}</span>
+        <span>${time.toFixed(1)} min</span>
+      </div>
+    `;
   }
 
 }
