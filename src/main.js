6d54003d91763f355c66be61404d6e03e711fc1a
Tomusdrw <tomusdrw@gmail.com>
2016-02-20 20:40:00 +0100

    Użycie Fetch API [Fetch API]

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import {TasksView} from './views/TasksView';
 
 const model = new TasksModel([]);
 
-//4/ 5. Używamy naszej pomocniczej funkcji
-fetch('data/tasks.json').then(
-  (tasks) => model.tasks = tasks,
-  (err) => console.error(err)
-);
+//6/ Okazuje się, że podobna funkcja jest już w przeglądarkach!
+fetch('data/tasks.json')
+  .then((response) => response.json())
+  .then(
+    (tasks) => model.tasks = tasks,
+    (err) => console.error(err)
+  );
 
 const activeView = new TasksView(
   document.querySelector('#todos'),
@@ -27,26 +29,3 @@ setInterval(() => {
   view.render(now);
   activeView.render(now);
 }, 500);
-
-//17/ 1. Pobieramy dany `url` i zwracamy `Promise`
-function fetch (url) {
-  // 2. Tworzymy nowy Promise
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open('GET', url, true);
-    req.onreadystatechange = (evt) => {
-      if (req.readyState !== 4) {
-        return;
-      }
-
-      if (req.status !== 200) {
-        // 3. Odrzucamy jeśli jest jakiś błąd...
-        return reject(req.status);
-      }
-      // 4. ...i rozwiązujemy, jeśli wszystko OK (pomijamy błędy z JSONa)
-      return resolve(JSON.parse(req.responseText));
-    };
-    req.send(null);
-  });
-}
-
