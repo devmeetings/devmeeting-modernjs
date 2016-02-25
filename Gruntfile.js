cdec77882ed62a446121aa84ce641636dcd19786
Michał Załęcki <zalecki.post@gmail.com>
2016-02-25 02:30:42 +0100

    Transpilacja ES6 do ES5 i bundle [babel i browserify]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,6 @@
 module.exports = function(grunt) {
 
-  //12/ Definicja tasków
   grunt.initConfig({
-    //11/ Definicja tasku odpowiada nazwie pluginu,
-    // a konfiguracja różni się pomiędzy pluginami
     htmlmin: {
       build: {
         options: {
@@ -14,12 +11,44 @@ module.exports = function(grunt) {
         }
       }
     },
+
+    //10/ Transpilacja ES6 do ES5
+    babel: {
+      //3/ Konfiguracja współdzielona pomiędzy wszystkimi podtaskami
+      options: {
+        sourceMap: true
+      },
+      //5/ Konfiguracja dla podtasku babel:build, może istnieć wiele podtasków
+      build: {
+        files: {
+          'build/app.js': 'src/app.js'
+        }
+      }
+    },
+
+    //12/ Łączenie plików i tworzenie wspólnego bundle
+    browserify: {
+      options: {
+        browserifyOptions : {
+          debug: true
+        }
+      },
+      build: {
+        files: {
+          /// Istotna jest kolejność jako, że browserify musi kożystać
+          /// z plików przygotowanych wcześniej przez babel
+          'build/app.js': ['build/app.js']
+        }
+      }
+    },
   });
 
-  /// Ładowanie używanych pluginów
+  //3/ Ładowanie używanych pluginów
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-browserify');
 
   /// Rejestracja domyślnego taska
-  grunt.registerTask('default', ['htmlmin']);
+  grunt.registerTask('default', ['htmlmin', 'babel', 'browserify']);
 
 };
