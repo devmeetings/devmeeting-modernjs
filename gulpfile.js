3add39c448e09a6678a9736a1f08ef0f110284fe
Michał Załęcki <zalecki.post@gmail.com>
2016-02-25 03:05:12 +0100

    Utworzenie domyślnego tasku [default task]

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 
-//3/ Taski gulpa to nie konfiguracja, a żywy kod
 gulp.task('hello', function() {
   console.log("Hello World!");
 });
+
+/// Utworzenie domyślnego tasku
+gulp.task('default', ['hello']);
