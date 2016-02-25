f2ee30de4b294643900ae210104faf1cb80d36cc
Michał Załęcki <zalecki.post@gmail.com>
2016-02-25 04:11:30 +0100

    Używaj babel-loader to transpilacji ES6 do ES5 [babel-loader: ES6 do ES5]

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,21 @@ var config = {
       minify: { collapseWhitespace: true },
       filename: "index.html"
     }),
-  ]
+  ],
+
+  //12/ Używaj loaderów do transofmacji plików podczas ich ładowania
+  module: {
+    loaders: [
+      {
+        /// zastosuj do plików, których ścieżka
+        test: /\.js$/,
+        /// nie stosuj do plików, których ścieżka
+        exclude: /node_modules/,
+        /// nazwa loadera
+        loader: "babel"
+      },
+    ]
+  }
 };
 
 module.exports = config;
