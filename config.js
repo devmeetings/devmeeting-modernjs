f60d2924b136986f8f488b5a08d090b1a1f634d0
Michał Załęcki <zalecki.post@gmail.com>
2016-02-25 03:45:51 +0100

    npm init

--- config.js
+++
@@ -1,8 +0,0 @@
-System.config({
-  defaultJSExtensions: true,
-  transpiler: false,
-  map: {
-    "css": "/cdn/systemjs/plugin-css/css-0.1.20.js",
-    "lodash": "/cdn/lodash.js/3.9.3/lodash.js",
-  }
-});
