6c621fc3ac06836f214e2721296410ac007c7cf7
Tomusdrw <tomusdrw@gmail.com>
2016-02-27 20:39:56 +0100

    Components slides update

--- components/009/config.js
+++ components/009/config.js
@@ -8,8 +8,6 @@ System.config({
   },
   map: {
     "css": "/cdn/systemjs/plugin-css/css-0.1.20.js",
-    "lodash": "/cdn/lodash.js/3.9.3/lodash.js",
-    //1/ Dodajemy zależności dla angulara
-    "angular2": "/cdn/angular.js/2.0.0-beta.7/angular2.dev.js"
+    "lodash": "/cdn/lodash.js/3.9.3/lodash.js"
   }
 });
