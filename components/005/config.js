b703317177799d37085e0fe5fee683f02d6274b1
Tomusdrw <tomusdrw@gmail.com>
2016-02-27 01:29:25 +0100

    Grunt-gulp update

---
+++ components/005/config.js
@@ -0,0 +1,15 @@
+System.config({
+  defaultJSExtensions: false,
+  transpiler: false,
+  meta: {
+    "*.css": {
+      loader: "css"
+    }
+  },
+  map: {
+    "css": "/cdn/systemjs/plugin-css/css-0.1.20.js",
+    "lodash": "/cdn/lodash.js/3.9.3/lodash.js",
+    //2/ Dodajemy zależności dla angulara
+    "angular": "/cdn/angular.js/1.5.0/angular.js"
+  }
+});
