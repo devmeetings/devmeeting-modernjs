f85a98b1debdc2863d53abbeb3ff2c162b70491e
Michał Załęcki <zalecki.post@gmail.com>
2016-02-28 22:36:55 +0100

    Początkowa konfiguracja [tsconfig i systemjs]

--- README.md
+++
@@ -1,2 +0,0 @@
-# devmeeting-modernjs
-ModernJS
---
+++ system.config.js
@@ -0,0 +1,5 @@
+//4/ Najprostrza konfiguracja musi jedynie uwzględniać główny moduł
+System.config({
+  baseURL: "./",
+  defaultJSExtensions: true
+});
