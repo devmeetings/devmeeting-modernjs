d4acf7fcdcfe5d12e86c1c6047a5cf3c1d05a2cf
Michał Załęcki <zalecki.post@gmail.com>
2016-02-28 23:54:52 +0100

    Renderowanie PartyForm [renderuj PartyForm]

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,15 @@
-//2/ Importuj zależności
+//3/ Importuj zależności
 import {Party} from "./party.class";
 import {PartyList} from "./party-list.class";
+import {PartyForm} from "./party-form.class";
 
-//3/ Stwórz i dodaj elementy do listy
 const partyList = new PartyList();
 partyList.add(new Party("Awesome party", new Date(), "52", "18"));
 partyList.add(new Party("Poor party", new Date(), "53", "21"));
 
-/// Renderuj lise
-document.getElementById("app").innerHTML = partyList.render();
+/// Utwórz instancje PartyForm
+const partyForm = new PartyForm();
+
+const app = document.getElementById("app");
+/// Renderuj PartyForm i PartyList
+app.innerHTML = partyForm.render() + partyList.render();
